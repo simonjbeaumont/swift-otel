955ff66a21b83631d34cb41cbc02d08b5876a19f
Si Beaumont <beaumont@apple.com>
2024-03-04 13:57:02 +0000

    experimental/wip: metric filter

--- Sources/OTel/Metrics/OTelMetricProducer/OTelMetricProducer.swift
+++ Sources/OTel/Metrics/OTelMetricProducer/OTelMetricProducer.swift
@@ -23,4 +23,67 @@ public protocol OTelMetricProducer: Sendable {
     /// - Seealso: [](https://github.com/open-telemetry/opentelemetry-specification/blob/v1.29.0/specification/metrics/sdk.md#produce-batch)
     /// - TODO: Consider adding metrics filter parameter (experimental in OTel 1.29.0)
     func produce() -> [OTelMetricPoint]
+
+//    @_spi(Experimental)
+//    func produce(filter: OTelMetricFilter) -> [OTelMetricPoint]
+}
+
+@_spi(Metrics)
+@_spi(Experimental)
+public struct OTelMetricFilterResult {
+    private enum Result {
+        case accept
+        case drop
+        case acceptPartial
+    }
+    private var result: Result
+    public static let accept = Self(result: .accept)
+    public static let drop = Self(result: .drop)
+    public static let acceptPartial = Self(result: .acceptPartial)
+}
+
+@_spi(Metrics)
+@_spi(Experimental)
+public struct OTelAttributesFilterResult {
+    private enum Result {
+        case accept
+        case drop
+    }
+    private var result: Result
+    public static let accept = Self(result: .accept)
+    public static let drop = Self(result: .drop)
+}
+
+@_spi(Metrics)
+@_spi(Experimental)
+public struct OTelMetricStreamKind {
+    private enum Kind {
+        case sum
+        case gauge
+        case histogram
+    }
+    private var kind: Kind
+    public static let sum = Self(kind: .sum)
+    public static let gauge = Self(kind: .gauge)
+    public static let histogram = Self(kind: .histogram)
+}
+
+
+@_spi(Metrics)
+@_spi(Experimental)
+public protocol OTelMetricFilter: Sendable {
+    func testMetric(
+        instrumentationScope: String,
+        name: String,
+        kind: OTelMetricStreamKind,
+        unit: String
+    ) -> OTelMetricFilterResult
+
+    func testAttributes(
+        instrumentationScope: String,
+        name: String,
+        kind: OTelMetricStreamKind,
+        unit: String,
+        attributes: Set<OTelAttribute>
+    ) -> OTelAttributesFilterResult
 }
